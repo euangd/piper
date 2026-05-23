f4d472547a36937de424878afd61008b530b9f70
euangd <gd@euan.my>
2026-05-23 23:50:40 +0700

    Update Hash.swift

--- PiperApp/Sources/Utils/Hash.swift
+++ PiperApp/Sources/Utils/Hash.swift
@@ -2,10 +2,12 @@
 // Copyright (c) 2026 Ihor Shevchuk
 
 import Foundation
-import CommonCrypto
+import Crypto
 
 extension Data {
     var md5String: String {
+        // If you really need MD5 for backward compatibility, use CommonCrypto
+        // Otherwise, consider migrating to SHA256
         var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
         self.withUnsafeBytes { bytes in
             _ = CC_MD5(bytes.baseAddress, CC_LONG(self.count), &digest)
